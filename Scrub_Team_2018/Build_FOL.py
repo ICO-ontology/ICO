b298dfd63603784f752fa4582a97ebcbf281800a
CooperStansbury <cstansbu@umich.edu>
2018-08-30 10:54:49 -0400

    (MAJOR) review of RUBRIC term development
    - finished an initial pass through RUBRIC defs
    - testing on non-critical tooling
    - update terms based on OBIB call
    - update terms based on GLBRN conference

--- Scrub_Team_2018/Build_FOL.py
+++ Scrub_Team_2018/Build_FOL.py
@@ -12,22 +12,25 @@ def makeFOL(file):
     source_file = os.path.basename(file)
     base = os.path.splitext(source_file)[0]
 
+    # load owlready graph object in order to print EquivalenceAxioms
     onto = ow.get_ontology(str("file://" + source_file)).load()
 
-    file_in = str(file)
-    file_out = str(base) + '_merged.owl'
+    # OWL file variables
+    OWL_file_in = str(file)
+    OWL_file_out = str(base) + '_merged.owl'
 
-    # # merge the ontology to get all subclass axioms
-    # robotMerge = 'robot merge --input ' + file_in + ' --output ' + file_out
-    # process = subprocess.Popen(robotMerge.split(), stdout=subprocess.PIPE)
-    # output, error = process.communicate()
+    # merge the ontology to get all subclass axioms
+    robotMerge = 'robot merge --input ' + OWL_file_in + ' --output ' + OWL_file_out
+    process = subprocess.Popen(robotMerge.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
 
     # rdflib naming conventions to query set-up
     g = rdflib.Graph()
-    # g = g.parse(file_out)
+    g = g.parse(OWL_file_out)
 
-    # for quick testing - TO REMOVE
-    g = g.parse(file)
+    # # for quick testing non-merged OWL file
+    # # TO REMOVE
+    # g = g.parse(file)
 
     # gather results from rdflib query
     qresults = g.query(
@@ -58,67 +61,74 @@ def makeFOL(file):
     # print all classes with import closure
     for row in qresults:
 
-        # gather different ontologies for separate files
+        # different ontologies for separate files
         shortIRI = str(row[0]).rsplit('/', 1)[-1]
         output = str('FOL_output/' + shortIRI.rsplit('_', 1)[0] + '_output.txt')
+        axiom_output = str('FOL_output/AXIOMS_output.txt')
 
-        # # print humand readable comments and PURL in case
-        # print("%  " + str(row[1]) + " is subclass of " + \
-        #     str(row[3]), file=open(output, "a"))
-        # print("%  entity: " + str(shortIRI), file=open(output, "a"))
-        #
-        # # print EquivalenceAxiom if they exist
-        # if row[4] != None:
-        #     print("% has EquivalenceAxiom: " + str(row[4]), file=open(output, "a"))
-        #
-        # print("all x (" + str(row[1]).title().replace(" ","") + "(x) -> " + \
-        #     str(row[3]).title().replace(" ","") + "(x)). \n", file=open(output, "a"))
+        # print humand readable comments and PURL in proven9 syntax
+        print("%  " + str(row[1]) + " is subclass of " + \
+            str(row[3]), file=open(output, "a"))
+        print("%  entity: " + str(shortIRI), file=open(output, "a"))
 
         # print EquivalenceAxiom if they exist
+        if row[4] != None:
+            print("% Has EquivalenceAxiom! ", file=open(output, "a"))
+            print("% Check FOL_output/AXIOMS_output.txt for EquivalenceAxiom", file=open(output, "a"))
+
+        print("all x (" + str(row[1]).title().replace(" ","") + "(x) -> " + \
+            str(row[3]).title().replace(" ","") + "(x)). \n", file=open(output, "a"))
 
         ## WARNING: need to check against empty list []
         ## not sure if checking for bnodes is good enough
 
-        # print entity PURL
-        print(str(row[2]))
+        # some entities don't have labels, handle
+        try:
+            # search for purl to get EquivalenceAxioms
+            search = str("*" + str(row[2]) + "*")
+            ow_IRI = onto.search_one(iri=search)
+            entity_name = str(ow_IRI.label)
+            axiom = str(ow_IRI.equivalent_to)
 
-        # search for purl to get EquivalenceAxioms
-        search = str("*" + str(row[2]) + "*")
-        ow_IRI = onto.search_one(iri=search)
-        axiom = str(ow_IRI.equivalent_to)
+            # remove brackets from axiom to parse individual purls
+            axiom = axiom.replace("[", "").replace("]", "")
 
-        # remove brackets
-        axiom = axiom.replace("[", "").replace("]", "")
+            # turn into a list
+            axiom = axiom.split()
 
-        # turn into a list
-        axiom = axiom.split()
+            # only print axioms that exist
+            if axiom != []:
 
-        # print
-        print("%  AXIOM: " + str(axiom))
-        print("% Words: ")
+                # print entity and axiom
+                print('ENTITY: ' + entity_name + ', ' + str(ow_IRI.name), file=open(axiom_output, "a"))
+                print("AXIOM: " + str(axiom), file=open(axiom_output, "a"))
 
-        # iterate through the axiom string to find rdfs:labels
-        for item in axiom:
-            # check if word contains base IRI
-            if item.__contains__(base.upper()):
+                # iterate through the axiom string to find rdfs:labels
+                for item in axiom:
 
-                # attempt to pull rdfs:labels for each IRI in the axiom, will need to replace
-                sub_axiom_search = str("*" + str(item) + "*")
+                    # search for each component
+                    sub_axiom_search = str("*" + str(item) + "*")
+                    sub_axiom_search = sub_axiom_search.replace("obo.", "")
+                    sub_axiom_IRI = onto.search_one(iri=sub_axiom_search)
 
-                # need to remove prefix
-                sub_axiom_IRI = onto.search_one(iri=sub_axiom_search.replace("obo.", ""))
+                    # get human readable label and purl for components
+                    if sub_axiom_IRI != None:
+                        sub_axiom_name = str(sub_axiom_IRI.label)
+                        print(sub_axiom_IRI.name, sub_axiom_name, file=open(axiom_output, "a"))
 
-                # get human readable label
-                name = str(sub_axiom_IRI.label)
-                print(name)
-        print()
+                # newline for readablility
+                print("\n", file=open(axiom_output, "a"))
 
-    # # clean up the dir
-    # remove_merged = "rm " + file_out
-    # process = subprocess.Popen(remove_merged.split(), stdout=subprocess.PIPE)
-    # output, error = process.communicate()
+        # catch entities without labels
+        except AttributeError:
+            continue
 
-    # print("Files printed to FOL_output/ directory.")
+    # clean up the dir
+    remove_merged = "rm " + OWL_file_out
+    process = subprocess.Popen(remove_merged.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
+
+    print("Files printed to FOL_Output/ directory.")
 
 if __name__ == "__main__":
 
