566aa491b6cc688f4cd1bff30bcc8e6d7b31d321
CooperStansbury <cstansbu@umich.edu>
2018-10-03 12:24:13 -0400

    update stubbed release process and dir
    update docker.md

--- tools/mass_update_tools/README.md
+++ tools/mass_update_tools/README.md
@@ -2,8 +2,7 @@
 This is a set of command line tools designed to extract terms and definitions from an OWL file into a `.csv`. The `.csv` file can be imported into a common application such as MS Excel in order to make changes to the content. Once changes have been made the OWL file can be updated for all terms that were changed. This is helpful for teams that want to make significant content changes to terms and definitions at once. The primary reason for including these is that they were used to revise content in `ico.owl` during the summer of 2018.
 
 ## Note on robot
-These tools are not as robust or system-independent as [robot](http://robot.obolibrary.org/). It is recommended that future work of this nature be conducted using [robot extract](http://robot.obolibrary.org/extract) and [robot template]
-(http://robot.obolibrary.org/template)
+These tools are not as robust or system-independent as [robot](http://robot.obolibrary.org/). It is recommended that future work of this nature be conducted using [robot extract](http://robot.obolibrary.org/extract) and [robot template](http://robot.obolibrary.org/template)
 
 ## `extract_onto.py`
 This tool takes one `.owl` file command line argument. It produces a `.csv` file with terms and definitions in the following format:
--- tools/release/ico_release.sh
+++ tools/release/ico_release.sh
@@ -1,12 +1,8 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
 
-# Requires robot to be installed
+# Requires robot to be installed locally
 command -v robot 1> /dev/null 2>&1 || \
   { echo >&2 "robot required but it's not installed.  Aborting."; exit 1; }
-
-# Requires robot to be installed
-command -v aspell 1> /dev/null 2>&1 || \
-  { echo >&2 "aspell required but it's not installed.  Aborting."; exit 1; }
 
 # Start by assuming it was the path invoked.
 THIS_SCRIPT="$0"
@@ -28,39 +24,32 @@ done
 # Get path to the scripts directory.
 SCRIPT_DIR=$(dirname "${THIS_SCRIPT}")
 
-PSDO="${SCRIPT_DIR}/../psdo.owl"
-LOG="${SCRIPT_DIR}/psdo_log.txt"
+
+# variables' set here
+# ICO="${SCRIPT_DIR}/../ico.owl"
+# LOG="${SCRIPT_DIR}/ico_log.txt"
 
 # add date to IRI version
 robot annotate \
-  --input ${PSDO} \
-  --ontology-iri "http://purl.obolibrary.org/obo/psdo.owl" \
-  --version-iri "http://purl.obolibrary.org/obo/`date '+%Y-%m-%d'`/psdo.owl" \
-  --output psdo-updated.owl
+  --input ${ICO} \
+  --ontology-iri "http://purl.obolibrary.org/obo/ico.owl" \
+  --version-iri "http://purl.obolibrary.org/obo/`date '+%Y-%m-%d'`/ico.owl" \
+  --output ico-updated.owl
 
 # run robot report, results to stdout and some to file
-robot report --input ${PSDO} --output ${LOG} | head -n 5
+robot report --input ${ICO} --output ${LOG} | head -n 5
 
 echo "Robot results can be seen here: ${LOG}"
 
 # update dictionary as part of release process
 ${SCRIPT_DIR}/make-dict.sh
 
-echo ""
-echo "The following lines contain misspelled words in the ontology, please fix them in ${PSDO}:"
-echo ""
-
-# spell checker
-< ${SCRIPT_DIR}/../DICTIONARY.md aspell list | grep -f /dev/stdin -C 1 ${SCRIPT_DIR}/../DICTIONARY.md
-
-echo ""
-
-# clean up
+# Final check before release
 while true; do
-    read -p "Are you ready to release this version?" yn
+    read -p "Are you ready to release this version? (y/n)" yn
     case $yn in
-        [Yy]* ) rm psdo.owl; mv psdo-updated.owl psdo.owl; rm catalog-v001.xml; echo 'RELEASE COMPLETE.'; break;;
-        [Nn]* ) rm psdo-updated.owl; exit;;
+        [Yy]* ) rm ico.owl; mv ico-updated.owl ico.owl; echo 'RELEASE COMPLETE. You can commit and '; break;;
+        [Nn]* ) rm ico-updated.owl; exit;;
         * ) echo "Please answer yes or no.";;
     esac
 done
