d21b355b7bb1b89af07237cf2fe418a6d82ff8e2
CooperStansbury <cstansbu@umich.edu>
2019-01-15 15:01:47 -0500

    fix stderr to logging file

--- tools/temporary_release_process.sh
+++ tools/temporary_release_process.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+## TODO: add developer comments
+
 # Requires robot to be installed
 command -v robot 1> /dev/null 2>&1 || \
   { echo >&2 "robot required but it's not installed.  Aborting."; exit 1; }
@@ -26,6 +28,8 @@ SCRIPT_DIR=$(dirname "${THIS_SCRIPT}")
 ICO="${SCRIPT_DIR}/../src/ontology/ico-edit.owl"
 LOG="${SCRIPT_DIR}/release_notes/`date '+%Y-%m-%d'`.txt"
 
+REASONER='ELK'
+
 MERGED="${SCRIPT_DIR}/intermediate_owl_artifacts/ICO-MERGED-`date '+%Y-%m-%d'`.owl"
 VERSIONED="${SCRIPT_DIR}/intermediate_owl_artifacts/ICO-VERSIONED-`date '+%Y-%m-%d'`.owl"
 RELEASE="ICO-RELEASE-`date '+%Y-%m-%d'`.owl"
@@ -34,48 +38,47 @@ RELEASE="ICO-RELEASE-`date '+%Y-%m-%d'`.owl"
 robot merge \
   --input ${ICO} \
   --include-annotations true \
-  --output ${MERGED}
+  --output ${MERGED} &> ${LOG}
 
 echo "MERGE DONE."
-echo ""
 
 # add date to IRI version
 robot annotate \
   --input ${MERGED} \
   --ontology-iri "http://purl.obolibrary.org/obo/ICO.owl" \
   --version-iri "http://purl.obolibrary.org/obo/`date '+%Y-%m-%d'`/ico.owl" \
-  --output ${VERSIONED}
+  --output ${VERSIONED} &> ${LOG}
 
 echo "VERSIONING DONE."
-echo ""
 
 # include reasoned triples
 robot reason \
 	--input ${VERSIONED} \
-  --reasoner ELK \
+  --reasoner ${REASONER} \
   --annotate-inferred-axioms true \
-  --output ${RELEASE} >> ${LOG}
+  --output ${RELEASE} &> ${LOG}
 
 echo "REASONING DONE."
-echo ""
 
 # run robot report, topsheet to stdout and details to file
-robot report --input ${RELEASE} --output ${LOG} | head -n 5
+robot report \
+  --input ${RELEASE} \
+  --output ${LOG} &> ${LOG} | head -n 5
 
-echo ""
 echo "REPORTING DONE."
 echo "Release notes can be found here: ${LOG}"
 
-echo ""
-
-# clean up
+# clean up, demand user input
 while true; do
     read -p "Would you like to remove intermediate artifacts (y/n)?" yn
     case $yn in
-        [Yy]* ) rm ${MERGED}; rm ${VERSIONED} break;;
+        [Yy]* ) rm ${MERGED}; rm ${VERSIONED}; break;;
         [Nn]* ) break;;
         * ) echo "Please answer yes (y) or no (n).";;
     esac
 done
 
 echo "RELEASE DONE."
+echo ""
+echo "##### IMPORTANT NOTE:"
+echo "Please rename and move ${RELEASE} in the root directory to 'src/ontology/ico_merged.owl'"
